cb5d37ea89bcbd749ffa60ff08f5e19c7de67057
onkvyawhare <onkvyawhare@gmail.com>
2024-10-28 14:27:07 +0530

    frontend code complete

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,37 @@
 
-function App() {
-  return (
-    <div className="App">
-     
-hhhhj
+// src/App.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
+import SearchBar from './components/Searchbar';
 
+const App = () => {
+  const [tasks, setTasks] = useState([]);
+  const [currentTask, setCurrentTask] = useState(null);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const fetchTasks = async () => {
+    const response = await axios.get('https://xoroassignment.onrender.com/tasks');
+    setTasks(response.data);
+  };
+
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const filteredTasks = tasks.filter(task =>
+    task.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="max-w-lg mx-auto p-4">
+      <h1 className="text-2xl font-bold text-center mb-4">Task Manager</h1>
+      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <TaskForm fetchTasks={fetchTasks} currentTask={currentTask} setCurrentTask={setCurrentTask} />
+      <TaskList tasks={filteredTasks} fetchTasks={fetchTasks} setCurrentTask={setCurrentTask} />
     </div>
   );
-}
+};
 
 export default App;
